502186d0c948f016a4a47e054b38f0aa931ea88c
CALEBCH0 <gmcho03@gmail.com>
2020-01-30 21:27:10 -0800

    relocate && set up virtualenv && optimize breach_wall

---
+++ breacher.py
@@ -0,0 +1,76 @@
+import platform
+import ctypes
+import os
+import sys
+# import gconf
+import wget
+
+
+def get_wall(wall_type):
+    url = 'https://i.imgur.com/WpZuQvO.png'
+    # 'https://ih0.redbubble.net/image.702318777.9332/poster,840x830,f8f8f8-pad,1000x1000,f8f8f8.jpg'
+    if wall_type == 'Windows':
+        wall_path = 'C://Users/Public/hen.jpg'
+        if os.path.isfile(wall_path):
+            os.remove(wall_path)
+        wget.download(url, wall_path)
+    elif wall_type == 'Linux':
+        wall_path = 'hoem/caleb/test_downloads/hen.jpg'
+        if os.path.isfile(wall_path):
+            os.remove(wall_path)
+        wget.download(url, wall_path)
+    else:
+        print("get_wall_error")
+    return wall_path
+
+
+def retract(wall_type):
+    if wall_type == 'Windows':
+        windows_breacher('C://Users/kmcho/OneDrive/Pictures/ELADISTORT.png')
+    elif wall_type == 'Linux':
+        print("unavailable")
+    elif wall_type == 'Darwin':
+        print("unavailable")
+    else:
+        print("retract_error")
+
+
+def breach_wall():
+    startup = input("start \n")
+    if startup == 'k':
+        set_wallpaper()
+    elif startup == 'r':
+        wall_type = platform.system()
+        print("retracting...")
+        retract(wall_type)
+        print("retracted.")
+    else:
+        print("breach_wall_error")
+
+
+def gnome_breacher(wall_path):
+    system('gsettings set org.gnome.desktop.background picture-uri file:///home/caleb/test_downloads/hen.jpg')
+
+
+def windows_breacher(wall_path):
+    wall_path = os.path.normpath(wall_path)
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, wall_path, 0)
+
+
+def set_wallpaper():
+    wall_type = platform.system()
+    print("getting wall...")
+    wall_path = get_wall(wall_type)
+    print("got it.")
+    print("breaching wall...")
+    if wall_type == 'Windows':
+        windows_breacher(wall_path)
+        print("wall breached.")
+    elif wall_type == 'Linux':
+        gnome_breacher(wall_path)
+        print("wall breached.")
+    elif wall_type == 'Darwin':
+        print("unavailale")
+    else:
+        print("set_wallpaper_error")
+
